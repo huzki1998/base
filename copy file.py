45a29f922cd89c4d4b00c04e9eefe6cd41e350be
huzki1998 <347361919@qq.com>
2023-04-24 19:00:54 +0800

    modify

--- copy file.py
+++ copy file.py
@@ -1,23 +1,33 @@
-# This Python script reads the target directory and the list of filenames to copy from two separate text files (des.txt and list.txt, respectively).
-# It then walks through the directory and its subdirectories, looking for files with names that match the ones in the list. For each matching file, it constructs the source and destination paths and copies the file using the shutil.copy() method.
-#
-# The script also creates a new file name for the copied file by appending the suffix "-new.xlsx" to the original file name, before the extension. This means that if a file in the list is named "file.xlsx", the copied file will be named "file-new.xlsx".
-#
-# The script checks if the destination directory exists and if not, prints an error message and exits. If a file cannot be copied for any reason, the script prints an error message indicating the file name and the reason for the failure.
-#
-# Overall, the script is a useful tool for copying a specific list of files from a directory and its subdirectories.
+# This is the code for copying specific files from a directory to a destination directory:
+
+# 1. Define the source and destination directories.
+# 2. Check if the destination directory exists. If not, create it using `os.makedirs()`.
+# 3. Check if the source directory exists. If not, print an error message and exit.
+# 4. Read the list of filenames to copy from the `list.txt` file using `open()`.
+# 5. Traverse through the source directory and all its subdirectories using `os.walk()`.
+# 6. For each file in the directory, check if its name is in the list of filenames to copy.
+# 7. If the file name is in the list, construct the source and destination file paths using `os.path.join()`.
+# 8. Copy the file from the source path to the destination path using `shutil.copy()`.
+# 9. If the copy is successful, print a success message. Otherwise, print an error message with the reason for the failure.
+
+# The code essentially copies a list of specified files from a source directory to a destination directory, and creates the destination directory if it does not exist.
 
 
 import os
 import shutil
 
-# Read the des.txt file to get the target directory.
-with open('des.txt', 'r', encoding='utf-8') as f:
-    des_dir = f.read().strip()
+location_path="D:\\tianshi\\国内release\\国内release策划端\\tables"
+des_dir="C:\\Users\\huzhk\\Desktop\\copy_file"
 
-# Check if the target directory exists.
+# Check if the target directory exists. If not, create it.
 if not os.path.isdir(des_dir):
-    print('directory doesn\'t exists')
+    # If the directory does not exist, create it and all its parent directories using os.makedirs()
+    os.makedirs(des_dir)
+    print('directory has been created：{}'.format(des_dir))
+
+# Check if the source directory exists.
+if not os.path.isdir(location_path):
+    print('Source directory does not exist')
     exit()
 
 # Read the list.txt file to get the list of filenames to copy.
@@ -25,7 +35,7 @@ with open('list.txt', 'r', encoding='utf-8') as f:
     file_list = f.read().splitlines()
 
 # Traverse through the specified directory and all its subdirectories.
-for root, dirs, files in os.walk(des_dir):
+for root, dirs, files in os.walk(location_path):
     for file_name in files:
         # If the file name is in the list, copy it to the target directory.
         if file_name in file_list:
@@ -38,7 +48,6 @@ for root, dirs, files in os.walk(des_dir):
                 # The shutil.copy() method can preserve file metadata such as permissions and creation time.
                 # If metadata preservation is not necessary, shutil.copy2() can also be used.
                 shutil.copy(src_path, dst_path)
-                print('success %s' % file_name)
+                print('success copied %s' % file_name)
             except Exception as e:
-                print('success %s fail：%s' % (file_name, str(e)))
-
+                print('copy failed %s reason：%s' % (file_name, str(e)))
